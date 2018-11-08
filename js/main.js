4f2da24433e7448f0f7aa7bcc1d27e114502c9b0
oljuanfe <ojuanferrero@gmail.com>
2018-11-08 22:26:41 +0100

    Add modal html, buttons listeners and prepare event functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const today = new Date();
+const day = today.getDay();
+const month = today.getMonth();
+const year = today.getFullYear();
+const dayOfWeek = today.getUTCDay();
+console.log('hoy', today, day, month, year, dayOfWeek);
+
+// selectores botones
+const displayModal = document.getElementById('display-modal-button');
+console.log('displaymodalbutton', displayModal);
+const addTask = document.getElementById('add-task');
+console.log('addtask', addTask);
+
+// Enseñar modal
+function showModal() {
+  console.log('Abriendo modal');
+}
+
+// Añadir nueva tarea
+function addNewTask() {
+  console.log('añadir tarea');
+}
+
+// Listener boton
+displayModal.addEventListener('click', showModal);
+
+addTask.addEventListener('click', addNewTask);
