b8405f90c59200951e81a78d92c5c22ef695033f
oljuanfe <ojuanferrero@gmail.com>
2018-11-11 13:18:57 +0100

    Add more styles, add proper date display to header

--- js/main.min.js
+++ js/main.min.js
@@ -2,27 +2,38 @@
 
 console.log('>> Ready :)');
 
-const today = new Date();
-const day = today.getDay();
-const month = today.getMonth();
-const year = today.getFullYear();
-const dayOfWeek = today.getUTCDay();
-console.log('hoy', today, day, month, year, dayOfWeek);
-
-// selectores botones
+// selectores elementos
 const dateWrapper = document.getElementById('date-wrapper');
+const dayOfMonth = document.getElementById('day');
+const dayFromWeek = document.getElementById('week-day');
+const monthYear = document.getElementById('month-year');
 const displayModal = document.getElementById('display-modal-button');
 const addTask = document.getElementById('add-task');
 const toDoList = document.getElementById('toDoList');
 const modal = document.getElementById('modal');
 const toDoTask = document.getElementById('to-do-task');
 
+// Fecha
+const today = new Date();
+const day = today.getDate();
+const month = today.getMonth();
+const year = today.getFullYear();
+const dayOfWeek = today.getDay();
+const monthString = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+const dayString = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+console.log('hoy', today, day, month, year, dayOfWeek);
+
 let newTaskContent;
 let allTasksToDo = [];
 
 
+// Enseñar fecha
+// dateWrapper.innerHTML = day + ' ' + month + ' ' + year;
+dayOfMonth.innerHTML = day;
+dayFromWeek.innerHTML = dayString[dayOfWeek];
+monthYear.innerHTML = monthString[month] + ', ' + year;
 
-dateWrapper.innerHTML = day + ' ' + month + ' ' + year;
+
 // Enseñar modal
 function showModal() {
   modal.classList.remove('hidden');
@@ -42,10 +53,14 @@ function createNewTask() {
   const newItem = document.createElement('li');
   const newLabel = document.createElement('label');
   const newCheckbox = document.createElement('input');
+  const newCheckMark = document.createElement('span');
+  newItem.setAttribute('class', 'to-do-list-item');
+  newLabel.setAttribute('class', 'checkbox-label');
   newCheckbox.setAttribute('type','checkbox');
   newCheckbox.setAttribute('class', 'checkbox');
+  newCheckMark.setAttribute('class', 'checkmark');
   const newContentLabel = document.createTextNode(newTaskContent);
-  newLabel.append(newCheckbox,newContentLabel);
+  newLabel.append(newCheckbox, newCheckMark, newContentLabel);
   newItem.appendChild(newLabel);
   console.log('creando componente tarea');
   return newItem;
